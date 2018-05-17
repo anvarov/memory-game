95f8dc94f68ead8463f5a065bcadc3a9f703a5a1
Akmal <akmalanvaroff@gmail.com>
2018-05-17 13:54:58 -0500

    fixed card pick function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 let prevCard = undefined;
 let pickedCardsArr = []
 let thisCard = undefined
+let time = 500
+
+switch(difficulty) {
+  case 'easy':
+    time = 1500
+  case 'medium':
+    time = 1000
+  case 'hard':
+    time = 500
+  default:
+    time = 1000
+}
 
 // function to shuffle an array to make it random, found from
 //https://www.w3resource.com/javascript-exercises/javascript-array-exercise-17.php
@@ -53,21 +65,30 @@ const addBoard = () => {
 }
 
 const pickCard = (e) => {
+  if (e.target.firstChild.style === undefined) {return}
+  if (pickedCardsArr.length === 2) {return}
   thisCard = e.target.firstChild
+  e.target.classList.add('animated', 'flipInY')
   thisCard.style.cssText = 'visibility: visible'
   pickedCardsArr.push(e.target)
   if (pickedCardsArr.length === 2) {
-    e.target.classList.remove('animated', 'shake', 'pulse')
-    pickedCardsArr[0].classList.remove('animated', 'shake', 'pulse')
+    moves++
+    e.target.classList.add('animated', 'flipInY')
+    // e.target.classList.remove('animated', 'shake', 'pulse', 'flipInY')
+    // pickedCardsArr[0].classList.remove('animated', 'shake', 'pulse', 'flipInY')
     if (thisCard.innerHTML !== pickedCardsArr[0].firstChild.innerHTML) {
       setTimeout(() => {
+        e.target.classList.remove('animated', 'shake', 'pulse', 'flipInY')
+        pickedCardsArr[0].classList.remove('animated', 'shake', 'pulse', 'flipInY')
         e.target.classList.add('animated', 'shake')
         thisCard.style.cssText = 'visibility: hidden';
         pickedCardsArr[0].classList.add('animated', 'shake')
         pickedCardsArr[0].firstChild.style.cssText = 'visibilty: hidden';
         pickedCardsArr = []
-      }, 500)
+      }, time)
     } else {
+      e.target.classList.remove('animated', 'shake', 'pulse', 'flipInY')
+      pickedCardsArr[0].classList.remove('animated', 'shake', 'pulse', 'flipInY')
       e.target.classList.add('animated', 'pulse');
       pickedCardsArr[0].classList.add('animated', 'pulse')
       pickedCardsArr = []
