96bf2d30e83e3f8506cfe08d0780ad5d97a1494e
anvarov <akmalanvaroff@gmail.com>
2018-05-29 13:55:03 -0500

    final version

--- js/app.js
+++ js/app.js
@@ -2,11 +2,23 @@ let prevCard = undefined;
 let pickedCardsArr = []
 let thisCard = undefined
 let time = 500
+
+//board items array
 const boardItems = ['build', 'build', 'alarm', 'alarm', 'copyright', 'copyright', 'pan_tool', 'pan_tool', 'verified_user', 'verified_user', 'track_changes', 'track_changes', 'stars', 'stars', 'search', 'search']
-let stars = 3
+
 let moves = 0
+
+//shuffled board will be stored in this array
 let generatedBoard = []
+
 let completed = 0
+let seconds = 0
+
+//timer function to countdown seconds from the start of a new game
+const timerFunc = () => {
+  ++seconds
+  document.getElementById('time-elapsed').innerHTML = `Time elapsed: ${seconds} seconds`
+}
 
 // function to shuffle an array to make it random, found from
 //https://www.w3resource.com/javascript-exercises/javascript-array-exercise-17.php
@@ -29,28 +41,48 @@ const shuffle = (arra1) => {
   return arra1;
 }
 
+// board generator function
 const boardGenerator = () => {
+  //shuffling the board using shuffle function
   const shuffledArr = shuffle(boardItems)
   for (let i = 0; i < 16; i++) {
     generatedBoard[i] = shuffledArr[i]
   }
 }
+
+//shuffling and adding the board to html
 const addBoard = () => {
-  if (document.querySelector('ul.deck')){
+  (() => {
+    // starting the timer
+    timer = window.setInterval(timerFunc, 1000)
+  })()
+  if (document.querySelector('ul.deck')) {
     const result = confirm('do you want to start a new game?')
     if (result) {
       document.querySelector('ul.deck').remove()
+      moves = 0;
+      seconds = 0;
+      document.getElementById('moves').innerText = 'Moves: 0'
+      document.getElementById('time-elapsed').innerHTML = 'Time elapsed: 0';
+      (() => {
+        //removing the timer
+        window.clearInterval(timer)
+      })()
     } else {
       return
     }
   }
+
+  // setting difficulty level
   if (document.getElementById('easy').checked) {
     time = 1500
-  } else if (document.getElementById('medium').checked){
+  } else if (document.getElementById('medium').checked) {
     time = 1000
   } else {
     time = 500
   }
+
+  //generate a board
   boardGenerator()
   let container = document.createElement('ul')
   for (let i = 0; i < 16; i++) {
@@ -65,11 +97,16 @@ const addBoard = () => {
   }
   container.setAttribute('class', 'deck')
   document.getElementById('app').appendChild(container)
-}
 
+}
+//pick card and compare to the other pick
 const pickCard = (e) => {
-  if (e.target.firstChild.style === undefined) {return}
-  if (pickedCardsArr.length === 2) {return}
+  if (e.target.firstChild.style === undefined) {
+    return
+  }
+  if (pickedCardsArr.length === 2) {
+    return
+  }
   thisCard = e.target.firstChild
   e.target.classList.add('animated', 'flipInY')
   thisCard.style.cssText = 'visibility: visible'
@@ -89,7 +126,7 @@ const pickCard = (e) => {
       }, time)
     } else {
       completed++
-      if (completed === 8){
+      if (completed === 8) {
         alert(`congratulations you did it in ${moves} moves`)
         document.querySelector('ul.deck').remove()
         return
@@ -101,5 +138,5 @@ const pickCard = (e) => {
       pickedCardsArr = []
     }
   }
-  document.getElementById('moves').innerText = `Moves ${moves}`
+  document.getElementById('moves').innerText = `Moves: ${moves}`
 }
